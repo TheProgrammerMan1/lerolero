75bb4d693a5c6c0cbff49355859d1a25f413a5be
Gabirel <gabriel.t.cunha@gmail.com>
2025-03-20 10:46:45 -0300

    modificacao no arquivo py

--- lerolero.py
+++ lerolero.py
@@ -9,4 +9,12 @@ parte1 = []
 parte2 = []
 parte3 = []
 
+lingua = int(input("Escolha a lingua: 1 - portugues , 2 - ingles\n")) 
+
+if lingua == 2:
+    parte1 = []
+    parte2 = []
+    parte3 = []
+
+
 print(random.choice(parte1),random.choice(parte2),random.choice(parte3))
