3f87ab14c960c8caeb2bcdca17c176db1b234a29
Lucas Aglio <lucas.aglio@usp.br>
2025-03-20 10:52:50 -0300

    adicionei as frases

--- lerolero.py
+++ lerolero.py
@@ -5,8 +5,14 @@ gera frases de efeito sem sentido
 """
 import random
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = ["O sitema em desenvolvimento",
+          "O novo protocolo de comunicação",
+          "O algoritimo otimizado"]
+parte2 = ["oferece garantias de precisão acima da média"
+        "possui excelente desenpenho",
+        "preenche uma lacuna significativa"]
+parte3 = ["nas aplicações em que se destina",
+          "em relação as demais opções do mercado",
+          ".Promovendo ampla vantagem competitiva"]
 
 print(random.choice(parte1),random.choice(parte2),random.choice(parte3))
